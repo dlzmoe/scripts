1d7918a3e9671b3ff811e25a1a9bfc7ebaa79ba8
98zi <anghunk@gmail.com>
2024-07-26 20:03:15 +0800

    Update linux.do 多功能脚本.js

--- linux.do 多功能脚本.js
+++ linux.do 多功能脚本.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         linux.do 多功能脚本
 // @namespace    https://github.com/dlzmoe/scripts
-// @version      0.0.3
-// @description  持续更新，有想要的功能，提就完事了
+// @version      0.0.4
+// @description  linux.do 多功能脚本，显示创建时间或将浏览器替换为时间，显示楼层数，隐藏签名尾巴，功能持续更新，欢迎提出。
 // @author       dlzmoe
 // @match        *://*.linux.do/*
 // @grant        GM_xmlhttpRequest
@@ -23,6 +23,8 @@
 
   var menu_ALL = [
     ['menu_showcreatetime', '帖子列表显示创建时间', '帖子列表显示创建时间', true],
+    ['menu_viewstotime', '将浏览量替换为创建时间', '将浏览量替换为创建时间', true],
+    ['menu_showfloors', '显示楼层数', '显示楼层数', true],
     ['menu_hidereplytail', '隐藏跟帖小尾巴签名', '隐藏跟帖小尾巴签名', false],
   ];
   var menu_ID = [];
@@ -105,12 +107,11 @@
     const day = String(date.getDate()).padStart(2, '0'); // 确保两位数
     const hours = String(date.getHours()).padStart(2, '0'); // 确保两位数
     const minutes = String(date.getMinutes()).padStart(2, '0'); // 确保两位数
-    const seconds = String(date.getSeconds()).padStart(2, '0'); // 确保两位数
 
     if (isToday) {
-      return `${hours}:${minutes}:${seconds}`;
+      return `${hours}:${minutes}`;
     } else {
-      return `${month}/${day} ${hours}:${minutes}:${seconds}`;
+      return `${month}/${day} ${hours}:${minutes}`;
     }
   }
 
@@ -157,24 +158,53 @@
           var color;
           var timeDiff = now - timestamp;
 
-          if (timeDiff < oneDay) {
-            color = '#45B5AA';
-            $(this).find('.post-activity').append(`<span class="linuxtime" style="color:${color}">（${formattedDate(timestamp)}）</span>`);
-          } else if (timeDiff < oneWeek) {
-            color = '#66A586';
-            $(this).find('.post-activity').append(`<span class="linuxtime" style="color:${color}">（${formattedDate(timestamp)}）</span>`);
-          } else if (timeDiff < oneMonth) {
-            color = '#CFA94A';
-            $(this).find('.post-activity').append(`<span class="linuxtime" style="color:${color}">（${formattedDate(timestamp)}）</span>`);
-          } else if (timeDiff < threeMonths) {
-            color = '#3e8ed2';
-            $(this).find('.post-activity').append(`<span class="linuxtime" style="color:${color}">（${formattedDate(timestamp)}）</span>`);
+
+          if (menu_value('menu_viewstotime')) {
+            $(this).siblings('.views').html('未知');
+            $('head').append(`<style>
+              .topic-list .views .number{opacity:0!important}
+              .topic-list .views{font-weight:400!important;white-space:nowrap!important;}
+              </style>`)
+            $('.topic-list th.views span').html('创建时间');
+            if (timeDiff < oneDay) {
+              color = '#45B5AA';
+              $(this).siblings('.views').html(`<span class="linuxtime" style="color:${color}">${formattedDate(timestamp)}</span>`);
+            } else if (timeDiff < oneWeek) {
+              color = '#66A586';
+              $(this).siblings('.views').html(`<span class="linuxtime" style="color:${color}">${formattedDate(timestamp)}</span>`);
+            } else if (timeDiff < oneMonth) {
+              color = '#CFA94A';
+              $(this).siblings('.views').html(`<span class="linuxtime" style="color:${color}">${formattedDate(timestamp)}</span>`);
+            } else if (timeDiff < threeMonths) {
+              color = '#3e8ed2';
+              $(this).siblings('.views').html(`<span class="linuxtime" style="color:${color}">${formattedDate(timestamp)}</span>`);
+            } else {
+              color = '#cccccc';
+              $(this).siblings('.views').html(`<span class="linuxtime" style="color:${color}"><img 
+              style="width: 20px;
+                    vertical-align: sub;" 
+              src="https://cdn.linux.do/uploads/default/original/3X/0/a/0a7634b834bc6ecef03ab57306dafd8475387155.png"> ${formattedDate(timestamp)}</span>`);
+            }
           } else {
-            color = '#cccccc';
-            $(this).find('.post-activity').append(`<span class="linuxtime" style="color:${color}">（<img 
-            style="width: 20px;
-                  vertical-align: sub;" 
-            src="https://cdn.linux.do/uploads/default/original/3X/0/a/0a7634b834bc6ecef03ab57306dafd8475387155.png"> ${formattedDate(timestamp)}）</span>`);
+            if (timeDiff < oneDay) {
+              color = '#45B5AA';
+              $(this).find('.post-activity').append(`<span class="linuxtime" style="color:${color}">（${formattedDate(timestamp)}）</span>`);
+            } else if (timeDiff < oneWeek) {
+              color = '#66A586';
+              $(this).find('.post-activity').append(`<span class="linuxtime" style="color:${color}">（${formattedDate(timestamp)}）</span>`);
+            } else if (timeDiff < oneMonth) {
+              color = '#CFA94A';
+              $(this).find('.post-activity').append(`<span class="linuxtime" style="color:${color}">（${formattedDate(timestamp)}）</span>`);
+            } else if (timeDiff < threeMonths) {
+              color = '#3e8ed2';
+              $(this).find('.post-activity').append(`<span class="linuxtime" style="color:${color}">（${formattedDate(timestamp)}）</span>`);
+            } else {
+              color = '#cccccc';
+              $(this).find('.post-activity').append(`<span class="linuxtime" style="color:${color}">（<img 
+              style="width: 20px;
+                    vertical-align: sub;" 
+              src="https://cdn.linux.do/uploads/default/original/3X/0/a/0a7634b834bc6ecef03ab57306dafd8475387155.png"> ${formattedDate(timestamp)}）</span>`);
+            }
           }
         }
       }
@@ -197,5 +227,26 @@
     $('head').append('<style>.topic-post .signature-img{display:none !important}</style>')
   }
   menu_hidereplytail();
+
+
+  // 显示楼层数
+  function menu_showfloors() {
+    if (!menu_value('menu_showfloors')) return;
+    $('head').append(`<style>
+    .topic-post{position:relative;}
+    .linuxfloor{display:flex;position:absolute;left:-32px;top:25px;color:#96aed0;width:30px;height:30px;align-items:center;justify-content:center;border-radius:6px;font-size:18px}
+
+      </style>`)
+    setInterval(() => {
+      $('.topic-post').each(function () {
+        const num = $(this).find('article').attr('id').replace(/^post_/, '');
+        if ($(this).find('.linuxfloor').length < 1) {
+          $(this).find('.topic-avatar').append(`<span class="linuxfloor">${num}</span>`)
+        }
+      })
+    }, 1000);
+
+  }
+  menu_showfloors();
 
 })();
