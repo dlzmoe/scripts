8f5cd8e81144090b73363dbbf6748632a4d0a549
dlzmoe <anghunk@gmail.com>
2024-10-24 13:54:10 +0800

    chore: 修改 nextchat-themes 代码

--- nextchat-themes/dist/nextchat-themes.user.js
+++ nextchat-themes/dist/nextchat-themes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nextchat-themes
 // @namespace    https://github.com/dlzmoe/UserScript
-// @version      0.0.2
+// @version      0.0.3
 // @author       dlzmoe
 // @description  NextChat 插件，修改 UI 主题！
 // @license      Apache-2.0
@@ -12,13 +12,13 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-(t=>{if(typeof GM_addStyle=="function"){GM_addStyle(t);return}const i=document.createElement("style");i.textContent=t,document.head.append(i)})(" :root{--primary: #333 !important}.home_container__4PEJZ{width:98vw!important;height:98vh!important;max-width:100%!important}.home_sidebar__fPZfq{background:#f9f9f9!important;box-shadow:none!important}.chat_chat-input__PQ_oF{min-height:130px!important}.settings_settings__427bK{max-width:1000px!important}.chat_chat-input-actions__mwYC_ .chat_chat-input-action__DMW7Y{width:auto!important}.chat_chat-input-actions__mwYC_ .chat_chat-input-action__DMW7Y .chat_text__TkPfN{opacity:1!important;transform:none!important}.chat_chat-message-avatar__3QeMq .chat_chat-message-edit__h58of button{display:none!important}.chat_chat-message-user__ZtTEj>.chat_chat-message-container__O_X8_>.chat_chat-message-item__dKqMl{background-color:#fff!important}.chat_chat-message-action-date__RsXTn{display:none!important}.ui-lib_list-item__YH0DO .ui-lib_list-header__RwThu .ui-lib_list-item-title__Fsa9c{font-size:16px}.ui-lib_list-item__YH0DO .ui-lib_list-header__RwThu .ui-lib_list-item-sub-title__jSgHb{font-size:14px;padding-top:6px}.input-range_input-range__SuxRd,.ui-lib_select-with-icon__L6FLF .ui-lib_select-with-icon-select__JhHwp{font-size:14px}.home_sidebar-tail__T2_u7{flex-direction:column-reverse;width:100%}.home_sidebar-actions__LcDT9:nth-child(2){width:100%!important;margin-bottom:10px}.home_sidebar-actions__LcDT9:nth-child(2) .button_shadow__G4m_0{width:100%!important;height:45px!important;background:#333!important;color:#fff!important}.home_sidebar-actions__LcDT9:nth-child(2) .button_shadow__G4m_0 *{font-size:16px!important}.home_sidebar-actions__LcDT9:nth-child(2) .button_shadow__G4m_0 .button_icon-button-icon__AMZta{transform:scale(1.2)}.home_sidebar-actions__LcDT9:nth-child(2) .button_shadow__G4m_0 svg{fill:#fff!important}.home_chat-item-info__9r6z_{display:none!important}.home_chat-item__Oblai{margin-bottom:5px!important}.home_chat-item-delete__3qV5m{top:13px!important;right:10px!important}.home_chat-item-title__sRstw{font-weight:500!important} ");
+(t=>{if(typeof GM_addStyle=="function"){GM_addStyle(t);return}const i=document.createElement("style");i.textContent=t,document.head.append(i)})(" :root{--primary: #333 !important}.home_sidebar-logo__Cd7hL svg path{fill:#999!important}.button_icon-button-text__my76e{font-size:14px!important}.home_container__4PEJZ{width:98vw!important;height:98vh!important;max-width:100%!important}.home_sidebar__fPZfq{background:#f9f9f9!important;box-shadow:none!important}.chat_chat-input__PQ_oF{min-height:130px!important}.settings_settings__427bK{max-width:1000px!important}.chat_chat-input-actions__mwYC_ .chat_chat-input-action__DMW7Y{width:auto!important}.chat_chat-input-actions__mwYC_ .chat_chat-input-action__DMW7Y .chat_text__TkPfN{opacity:1!important;transform:none!important}.chat_chat-message-avatar__3QeMq .chat_chat-message-edit__h58of button{display:none!important}.chat_chat-message-avatar__3QeMq .no-dark svg g rect{fill:#fff!important}.chat_chat-message-user__ZtTEj>.chat_chat-message-container__O_X8_>.chat_chat-message-item__dKqMl{background-color:#fff!important}.chat_chat-message-action-date__RsXTn{display:none!important}.ui-lib_list-item__YH0DO .ui-lib_list-header__RwThu .ui-lib_list-item-title__Fsa9c{font-size:16px}.ui-lib_list-item__YH0DO .ui-lib_list-header__RwThu .ui-lib_list-item-sub-title__jSgHb{font-size:14px;padding-top:6px}.input-range_input-range__SuxRd,.ui-lib_select-with-icon__L6FLF .ui-lib_select-with-icon-select__JhHwp{font-size:14px}.home_sidebar-tail__T2_u7{flex-direction:column-reverse;width:100%}.home_sidebar-actions__LcDT9>.home_sidebar-action__IVfyJ:nth-child(3){display:none!important}.home_sidebar-actions__LcDT9:nth-child(2){width:100%!important;margin-bottom:10px}.home_sidebar-actions__LcDT9:nth-child(2) .button_shadow__G4m_0{width:100%!important;height:40px!important;background:#333!important;color:#fff!important}.home_sidebar-actions__LcDT9:nth-child(2) .button_shadow__G4m_0 *{font-size:16px!important}.home_sidebar-actions__LcDT9:nth-child(2) .button_shadow__G4m_0 .button_icon-button-icon__AMZta{transform:scale(1.2)}.home_sidebar-actions__LcDT9:nth-child(2) .button_shadow__G4m_0 svg{fill:#fff!important}.home_chat-item-info__9r6z_{display:none!important}.home_chat-item__Oblai{margin-bottom:5px!important;position:relative!important}.home_chat-item-delete__3qV5m{top:50%!important;transform:translateY(-50%)!important;right:10px!important}.home_chat-item-title__sRstw{font-weight:500!important}#webdavBtn{position:fixed;bottom:25px;left:85px;display:flex;align-items:center;display:none}#webdavBtn button{margin-right:10px!important}#webdavBtn .button_icon-button-icon__AMZta{width:auto!important} ");
 
 (function (vue) {
   'use strict';
 
   const name = "nextchat-themes";
-  const version = "0.0.2";
+  const version = "0.0.3";
   const author = "dlzmoe";
   const description = "NextChat plug-in, modify the UI theme!";
   const type = "module";
@@ -59,17 +59,40 @@
     return target;
   };
   const _sfc_main = {
-    methods: {},
+    methods: {
+      addWebdavBtn() {
+      }
+    },
     created() {
       console.log(
         `%c ${packageJson.name} %c 已开启 `,
         "padding: 2px 1px; color: #fff; background: #606060;",
         "padding: 2px 1px; color: #fff; background: #42c02e;"
       );
+      this.addWebdavBtn();
     }
   };
+  const _hoisted_1 = { id: "webdavBtn" };
+  const _hoisted_2 = /* @__PURE__ */ vue.createElementVNode("button", {
+    class: "button_icon-button__VwAMf undefined button_shadow__G4m_0 clickable undefined",
+    role: "button",
+    "aria-label": "ChatGPT"
+  }, [
+    /* @__PURE__ */ vue.createElementVNode("div", { class: "button_icon-button-icon__AMZta false" }, "上传到云端")
+  ], -1);
+  const _hoisted_3 = /* @__PURE__ */ vue.createElementVNode("button", {
+    class: "button_icon-button__VwAMf undefined button_shadow__G4m_0 clickable undefined",
+    role: "button",
+    "aria-label": "ChatGPT"
+  }, [
+    /* @__PURE__ */ vue.createElementVNode("div", { class: "button_icon-button-icon__AMZta false" }, "同步到本地")
+  ], -1);
+  const _hoisted_4 = [
+    _hoisted_2,
+    _hoisted_3
+  ];
   function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-    return null;
+    return vue.openBlock(), vue.createElementBlock("div", _hoisted_1, _hoisted_4);
   }
   const App = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
   const app = vue.createApp(App);
--- nextchat-themes/src/app.vue
+++ nextchat-themes/src/app.vue
@@ -1,15 +1,37 @@
-<template></template>
+<template>
+  <div id="webdavBtn">
+    <button
+      class="button_icon-button__VwAMf undefined button_shadow__G4m_0 clickable undefined"
+      role="button"
+      aria-label="ChatGPT"
+    >
+      <div class="button_icon-button-icon__AMZta false">上传到云端</div>
+    </button>
+
+    <button
+      class="button_icon-button__VwAMf undefined button_shadow__G4m_0 clickable undefined"
+      role="button"
+      aria-label="ChatGPT"
+    >
+      <div class="button_icon-button-icon__AMZta false">同步到本地</div>
+    </button>
+  </div>
+</template>
 
 <script>
 import packageJson from "../package.json";
 export default {
-  methods: {},
+  methods: {
+    addWebdavBtn() {},
+  },
   created() {
     console.log(
       `%c ${packageJson.name} %c 已开启 `,
       "padding: 2px 1px; color: #fff; background: #606060;",
       "padding: 2px 1px; color: #fff; background: #42c02e;"
     );
+
+    this.addWebdavBtn();
   },
 };
 </script>
